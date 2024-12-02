688a1da726d60c00dd2b5d3fc26f480e244937ae
Decoder76 <lokeshkjayswal999@gmail.com>
2024-12-02 16:37:00 +0530

    new update e2e_test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '8t456d',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
